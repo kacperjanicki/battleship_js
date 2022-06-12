eef339c0714e8972b635626009d73ce3a553a6d3
kacperjanicki <kacperekjanicki9@gmail.com>
2022-06-12 17:04:17 +0200

    matrix added, horizontal support

--- script.js
+++ script.js
@@ -6,40 +6,55 @@ for(let i=0;i<=10;i++){                         //create 10 rows
     grid.appendChild(container)
 }
 
-const rows = document.querySelectorAll('.row')
-rows.forEach( row=>{
-    for(let i=0;i<=10;i++){                     //create 10 buttons in each row
-        var btn = document.createElement('button')
-        btn.setAttribute('data',`${i+1}`)
-        row.appendChild(btn)
-        btn.addEventListener('click',(e)=>{
-            console.log(e.target)     //button clicked
-            const parent_div = e.target.parentElement    //find parent div
-            var divs=document.querySelectorAll('.row')
-            divs.forEach(div=>{
-                if(div.getAttribute('data')==parent_div.getAttribute('data')){
-                    found_div = div
-                }
-            })
 
-            var btns = document.querySelectorAll(`.row[data="${parent_div.getAttribute('data')}"] > button`)
-            btns.forEach(button=>{
-                var j=0;
-                for(let i=e.target.getAttribute('data');i<=11; i++){
-                    if(j<=4){
-                        console.log(i)
-                        if(button.getAttribute('data')==i){
-                            button.classList.add('target')
-                        }
+var ships={'Carrier':5,'Battleship':4,'Destroyer':3,'Submarine':3,'Patrol Boat':2}
+console.table(ships)
+var user_matrix = []
+for(let i=0;i<=10;i++){
+    user_matrix.push(['.','.','.','.','.','.','.','.','.','.','.'])
+}
+
+
+function board(){
+    var click_count=0;
+    const rows = document.querySelectorAll('.row')
+    rows.forEach( row=>{
+        for(let i=0;i<=10;i++){                     //create 10 buttons in each row
+            var btn = document.createElement('button')
+            btn.setAttribute('data',`${i+1}`)
+            row.appendChild(btn)
+            btn.addEventListener('click',(e)=>{
+                click_count+=1
+                console.log(click_count)
+                console.log(e.target)     //button clicked
+                const parent_div = e.target.parentElement    //find parent div
+
+                var divs=document.querySelectorAll('.row')
+                divs.forEach(div=>{
+                    if(div.getAttribute('data')==parent_div.getAttribute('data')){
+                        found_div = div
                     }
-                    j+=1
-                    
-                }
+                })
 
-                button.getAttribute('data')
-                
+                var btns = document.querySelectorAll(`.row[data="${parent_div.getAttribute('data')}"] > button`)
+                btns.forEach(button=>{
+                    var j=0;
+                    for(let i=e.target.getAttribute('data');i<=11; i++){
+                        if(j<=4){
+                            if(button.getAttribute('data')==i){
+                                button.classList.add('target')
+                                user_matrix[parent_div.getAttribute('data')-1][i-1] = 'x'
+                            }
+                        }
+                        j+=1     
+                    }
+                })
+                console.log(Array.from(user_matrix))
             })
-        })
-    }
-} )
+        }
+    } )
+    
+}
+    
+board()
 
